49d98f12fbdbd9acf8c315da13de4a8c80aff4a2
fangao <fangao@staruniongame.com>
2025-01-13 11:20:43 +0800

    feat: 优化

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next'
+
+const Sitemap = () => null
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://yourdomain.com</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+    </urlset>`
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default Sitemap 
